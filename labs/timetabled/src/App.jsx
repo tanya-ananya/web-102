93156e6dc6faa56102473a61a697624a3425d692
tanya-ananya <taaruni.ananya@gmail.com>
2025-02-27 22:33:35 -0500

    lab1 update 1

---
+++ labs/timetabled/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Reading Schedule</h1>
+      <h2>A 7-day scheduling and tracking companion for readers 📚</h2>
+    </div>
+  )
+}
+
+export default App
